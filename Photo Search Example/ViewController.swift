34168ec98040d638ee79fe47b1eda7aca7a52ec9
Aanya Jhaveri <Aanya@aanyas-laptop.local>
2017-08-08 19:52:28 -0500

    Initial Commit

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -7,18 +7,40 @@
 //
 
 import UIKit
+import AFNetworking
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let manager = AFHTTPSessionManager()
+        let searchParameters:[String:Any] = ["method": "flickr.photos.search",
+                                             "api_key": "e8b64840adaafc462120e621e5f01a67",
+                                             "format": "json",
+                                             "nojsoncallback": 1,
+                                             "text": "dogs",
+                                             "extras": "url_m",
+                                             "per_page": 5]
+        
+        manager.get("https://api.flickr.com/services/rest/",
+                    parameters: searchParameters,
+                    progress: nil,
+                    success: { (operation: URLSessionDataTask, responseObject: Any?) in
+                        if let responseObject = responseObject {
+                            print("Response: " + (responseObject as AnyObject).description)
+                            
+                        }
+        }) {(operation:URLSessionDataTask?, error: Error) in
+            print("Error: " + error.localizedDescription)
+        }
     }
-
+    
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+        }
 
 
 }
---
+++ bridge.h
@@ -0,0 +1,9 @@
+//
+//  bridge.h
+//  Photo Search Example
+//
+//  Created by Aanya Jhaveri on 8/8/17.
+//  Copyright © 2017 Aanya Jhaveri. All rights reserved.
+//
+
+#import "AFNetworking/AFNetworking.h"
